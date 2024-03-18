f4601add933b80e47ff6c24b14af1856e86f7f13
james9152000 <lanricjames@gmail.com>
2024-03-18 09:28:30 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="vendor/bootstrap-4.4.1-dist/css/bootstrap.min.css">
+
+     <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"></script>
+  
+    <script>
+          var controller = new ScrollMagic.Controller();
+    </script>
+
+    <link rel="shortcut icon" href="assets/logo/NEDA.png" type="image/x-icon">
+    <title>NEDA REGION 3</title>
+
+</head>
+<body>
+
+    <div class="navbar navbar-expand-lg navbar-dark sticky-top" id="header">
+
+        <div class="container d-flex justify-content-start">
+            <!-- <img src="assets/logo/Bagong Pilipinas Logo.png" alt=""> -->
+            <img src="assets/header.png" alt="">
+        </div>            
+
+    </div>
+
+    <header id="intro">
+        <div class="container">
+            <div class="intro-text">
+              <div class="intro-lead-in" style="text-shadow: 3px 3px 4px rgba(0, 0, 0, 1);">Welcome NEDA 3 Central Luzon</div>
+              <div class="intro-heading text-uppercase" style="text-shadow: 3px 3px 4px rgba(0, 0, 0, 1);">It's Nice To Meet You</div>
+            </div>
+        </div>
+    </header>
+
+    <section class="bg-light p-5" id="portfolio">
+
+        <div class="container-fluid">
+
+            <div class="row">
+              <div class="col-lg-12 pb-3 text-center">
+                  <h2 class="section-heading text-uppercase">Information Systems</h2>
+                  <h3 class="section-subheading text-muted"></h3>
+              </div>
+            </div>
+
+            <div class="row border d-flex flex-wrap justify-content-start p-2">
+
+                <div class="col-md-4 col-sm-6 portfolio-item">
+                    <!-- <a class="portfolio-link"  target = "_blank" href="http://172.16.114.3/LOGBOOK/"> -->
+                    <a class="portfolio-link"  target = "_blank" href="./LOGBOOK/">
+                      <div class="portfolio-hover">
+                          <div class="portfolio-hover-content">
+                          
+                          </div>
+                      </div>
+                      <div style="display: flex; align-items: center; justify-content: center;"><img class="img-fluid" src="assets/service/elogbookNew.png" alt="" style="height: 50%; width: 50%;"></div>
+                    </a>
+                    <div class="portfolio-caption">
+                      <h4>eLogbook</h4>
+                      <p class="text-muted">Electronic Logbook</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 portfolio-item">
+                    <!-- <a class="portfolio-link"  target = "_blank" href="http://172.16.114.3/PERSONNEL/"> -->
+                    <a class="portfolio-link"  target = "_blank" href="./PERSONNEL/">
+                      <div class="portfolio-hover">
+                          <div class="portfolio-hover-content">
+                          
+                          </div>
+                      </div>
+                      <div style="display: flex; align-items: center; justify-content: center;"><img class="img-fluid" src="assets/service/eDTRNew.png" alt="" style="height: 50%; width: 50%;"></div>
+                    </a>
+                    <div class="portfolio-caption">
+                      <h4>eDTR</h4>
+                      <p class="text-muted">Electronic Daily Time Record</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 portfolio-item">
+                    <a class="portfolio-link"  target = "_blank" href="https://erpmes-nro3.neda.gov.ph/">
+                      <div class="portfolio-hover">
+                          <div class="portfolio-hover-content">
+                          
+                          </div>
+                      </div>
+                      <div style="display: flex; align-items: center; justify-content: center;"><img class="img-fluid" src="assets/service/eRPMESNew.png" alt="" style="height: 50%; width: 50%;"></div>
+                    </a>
+                    <div class="portfolio-caption">
+                      <h4>eRPMES</h4>
+                      <p class="text-muted">Electronic Regional Project Monitoring and Evaluation System</p>
+                    </div>
+                </div>            
+                <div class="col-md-4 col-sm-6 portfolio-item">
+                    <a class="portfolio-link"  target = "_blank" href="https://nedasapasig-my.sharepoint.com/_forms/default.aspx">
+                      <div class="portfolio-hover">
+                          <div class="portfolio-hover-content">
+                          
+                          </div>
+                      </div>
+                      <div style="display: flex; align-items: center; justify-content: center;"><img class="img-fluid" src="assets/service/document.png" alt="" style="height: 50%; width: 50%;"></div>
+                    </a>
+                    <div class="portfolio-caption">
+                      <h4>Forms</h4>
+                      <p class="text-muted">Download the Government Document</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 portfolio-item">
+                  <a class="portfolio-link"  target = "_blank" href="https://nedasapasig-my.sharepoint.com/personal/ebpurzuelo_neda_gov_ph/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Febpurzuelo%5Fneda%5Fgov%5Fph%2FDocuments%2FDocuments%2F3%2E%20Document%20Control%2FQMS%5FPDF%2F1%5FNEDA%203%20QMS%20Table%20of%20Contents%2Epdf&parent=%2Fpersonal%2Febpurzuelo%5Fneda%5Fgov%5Fph%2FDocuments%2FDocuments%2F3%2E%20Document%20Control%2FQMS%5FPDF&ga=1">
+                    <div class="portfolio-hover">
+                        <div class="portfolio-hover-content">
+                        
+                        </div>
+                    </div>
+                    <div style="display: flex; align-items: center; justify-content: center;"><img class="img-fluid" src="assets/service/qms.png" alt="" style="height: 50%; width: 50%;"></div>
+                  </a>
+                  <div class="portfolio-caption">
+                    <h4>Quality Management System</h4>
+                    <p class="text-muted">Ensuring quality standards are met.</p>
+                  </div>
+                </div>
+
+            </div>
+
+    </section>
+
+    <!-- Footer -->
+
+  <footer class="text-center text-lg-start bg-body-tertiary text-muted">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+
+      <!-- Left -->
+      <div class="me-5 d-none d-lg-block">
+        <span>Get connected with us on social networks:</span>
+      </div>
+      <!-- Left -->
+
+      <!-- Right -->
+
+      <div>
+        <a href="https://www.facebook.com/nedaregion3" target="_blank" class="me-4 text-reset">
+          <i class="fab fa-facebook-f h3 mr-2"></i>
+        </a>
+        <a href="https://twitter.com/intent/post?text=Project%20Monitoring%20and%20Evaluation%20(M%26E)%20https%3A%2F%2Fnro3.neda.gov.ph" target="_blank" class="me-4 text-reset">
+          <i class="fab fa-twitter h3 mr-2"></i>
+        </a>
+        <a href="https://www.youtube.com/@nedaregionaloffice3534" target="_blank" class="me-4 text-reset">
+          <i class="fab fa-youtube h3 mr-2"></i>
+        </a>
+      </div>
+
+      <!-- Right -->
+
+    </section>
+    
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="">
+      <div class="container-fluid text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              <img src="assets/logo/NEDA.png" alt="" width="110" height="110">
+            </h6>
+            <p class="text-justify">
+              National Economic and Development Authority
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              About Us
+            </h6>
+            <p>
+              <a href="https://nro3.neda.gov.ph/vision-and-mission-2/" target="_blank" class="text-reset">Mission, Vision and Quality Policy</a>
+            </p>
+            <p>
+              <a href="https://nro3.neda.gov.ph/about-neda/menu/functions/" target="_blank" class="text-reset">Functions</a>
+            </p>
+            <p>
+              <a href="https://nro3.neda.gov.ph/about-neda/menu/directors-corner/" target="_blank" class="text-reset">Director's Corner</a>
+            </p>
+            <p>
+              <a href="https://nro3.neda.gov.ph/eo-no-230-reorganizing-the-national-economic-and-development-authority/" target="_blank" class="text-reset">Legal Bases</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Resources
+            </h6>
+            <p>
+              <a href="https://nro3.neda.gov.ph/publications/" target="_blank" class="text-reset">Publications and Reports</a>
+            </p>
+            <p>
+              <a href="https://nro3.neda.gov.ph/monitoring-evaluation/" target="_blank" class="text-reset">Project Monitoring and Reports(M&E)</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-5 col-lg-5 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+            <p class="text-justify"><i class="fas fa-home me-3 mr-2"></i>NEDA Region 3, Diosdado Macapagal Government Center, Maimpis, City of San Fernando, Pampanga. Philippines 2000</p>
+            <p>
+              <i class="fas fa-envelope me-3 mr-2"></i>
+              nro3@neda.gov.ph
+            </p>
+            <p><i class="fas fa-phone me-3 mr-2"></i>(045) 963-5012</p>
+            <p><i class="fas fa-print me-3 mr-2"></i>963-5993 / 455-4110</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+      © 2024 Copyright:
+      <a class="text-reset fw-bold" target="_blank" href="https://nro3.neda.gov.ph/">National Economic and Development Authority.  All rights reserved.</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+
+  <script>
+    		// build scenes
+		var revealElements = document.getElementsByClassName("portfolio-item");
+		for (var i=0; i<revealElements.length; i++) { // create a scene for each element
+			new ScrollMagic.Scene({
+            triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
+            offset: 50,												 // start a little later
+            triggerHook: 0.9,
+          })
+          .setClassToggle(revealElements[i], "visible") // add class toggle
+          //.addIndicators({name: "digit " + (i+1) }) // add indicators (requires plugin)
+          .addTo(controller);
+		}
+  </script>
+    
+</body>
+</html>
